2e512c5a532ec570c298820dc06e0b969d1e0510
Ping-Yen Yu <cookie2000891109@gmail.com>
2024-04-09 00:31:19 +0800

    Delete gpu.py

--- gpu.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    device = torch.cuda.get_device_name()
-    print("GPU CUDA is available.")
-    print("GPU Model:", device)
-else:
-    print("GPU CUDA is not available.")
